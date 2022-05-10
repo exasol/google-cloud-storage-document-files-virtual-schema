e7f9203edc29a82d07aea8542f12c372ccee062f
jakobbraun <jakob.braun@posteo.de>
2022-05-10 11:27:07 +0200

    bug/10: Upgraded dependencies to fix security issue in Postgres JDBC driver (#11)
    * bug/10: Upgraded dependencies to fix security issue in Postgres JDBC driver

--- src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
@@ -27,7 +27,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class IntegrationTestSetup implements AutoCloseable {
-    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-6.0.0-google-cloud-storage-1.0.0.jar";
+    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-6.0.1-google-cloud-storage-1.0.1.jar";
     private final ExasolTestSetup exasolTestSetup;
     private final Connection connection;
     private final Statement statement;
