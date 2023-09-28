50101618f13cb1360023fbe44083d624313a090e
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-09-28 14:20:22 +0200

    #37: Fix CVE-2023-43642, CVE-2023-42503 & CVE-2023-4759 (#38)

--- src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
@@ -28,7 +28,7 @@ import jakarta.json.*;
 public class IntegrationTestSetup implements AutoCloseable {
     private static final Logger LOG = Logger.getLogger(IntegrationTestSetup.class.getName());
 
-    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-7.3.3-google-cloud-storage-1.3.2.jar";
+    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-7.3.4-google-cloud-storage-1.3.3.jar";
     private final ExasolTestSetup exasolTestSetup;
     private final Connection connection;
     private final Statement statement;
