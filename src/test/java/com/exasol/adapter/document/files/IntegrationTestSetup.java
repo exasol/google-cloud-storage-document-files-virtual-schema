28861564b4f1b4b97f190868abc22763fae1cd4a
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-06-17 10:57:19 +0200

    #58: Update dependencies

--- src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
@@ -28,7 +28,7 @@ import jakarta.json.*;
 public class IntegrationTestSetup implements AutoCloseable {
     private static final Logger LOG = Logger.getLogger(IntegrationTestSetup.class.getName());
 
-    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.0.4-google-cloud-storage-2.0.4.jar";
+    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.1.0-google-cloud-storage-2.1.0.jar";
     private final ExasolTestSetup exasolTestSetup;
     private final Connection connection;
     private final Statement statement;
